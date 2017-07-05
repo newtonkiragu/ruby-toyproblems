2cc036dec0153e2b560a83cc511b87ff5b25d53c
EdwardMudaida <eddieigo8@gmail.com>
2017-07-05 09:43:03 +0300

    Dehydrated factorial solution

--- 01-factorial.rb
+++ 01-factorial.rb
@@ -18,7 +18,7 @@ def factorial(n)
   # else
   #   return n * factorial(n - 1)
   # end
-  n == 0? n: n * factorial(n - 1)
+  n == 0? 1: n * factorial(n - 1)
 end
 
 puts("factorial(0) == 1: #{factorial(0) == 1}")
@@ -26,3 +26,4 @@ puts("factorial(1) == 1: #{factorial(1) == 1}")
 puts("factorial(2) == 2: #{factorial(2) == 2}")
 puts("factorial(3) == 6: #{factorial(3) == 6}")
 puts("factorial(4) == 24: #{factorial(4) == 24}")
+puts factorial(5)
